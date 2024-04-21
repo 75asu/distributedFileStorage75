390c324b832bee11ba9334cd5e6a0866a79b31a0
Asutosh Panda <78539272+75asu@users.noreply.github.com>
2024-04-21 11:15:52 +0530

    add tcp peer setup
    expected output: can connect from a peer in another terminal

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"distributedFileStorage75/p2p"
+	"log"
+)
+
+func main() {
+	tr := p2p.NewTCPTransport(":3000")
+
+	if err := tr.ListenAndAccept(); err != nil {
+		log.Fatal(err)
+	}
+
+	select {}
+}
